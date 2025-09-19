16cf09315b92a8c0291b848fbab99e9b22936362
Johnsbeltran <jbeltran@cesde.net>
2025-09-19 01:38:01 -0500

    Update ejercicio_10.js

---
+++ ejercicios/ejercicio_10.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  if (n < 0) {
+    return "No hay factorial negativo";
+  } else if (n === 0) {
+    return 1;
+  }
+  let resultado = 1;
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
+}
